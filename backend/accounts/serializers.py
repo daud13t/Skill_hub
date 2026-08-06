e7d041c2f1cd82938a51cf29ed80b572a54857e1
daud13 <daud13t@gmail.com>
2026-08-06 13:29:21 +0500

    Authentication proved

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Profile
 
 class RegisterSerializer(serializers.ModelSerializer):
       password = serializers.CharField(write_only=True)
@@ -14,4 +15,10 @@ class RegisterSerializer(serializers.ModelSerializer):
                   email = validated_data['email'],
                   password = validated_data['password']
             )
+            Profile.objects.create(user=user)
             return user
+
+class ProfileSerializer(serializers.ModelSerializer):
+      class Meta:
+            model = Profile
+            fields = ['bio','current_mode']
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer,ProfileSerializer
+from .models import Profile
 # Create your views here.
 
 class RegisterView(generics.CreateAPIView):
       queryset = User.objects.all()
       serializer_class = RegisterSerializer
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+      serializer_class = ProfileSerializer
+      permission_classes = [IsAuthenticated]
+
+      def get_object(self):
+            return self.request.user.profile
+            
